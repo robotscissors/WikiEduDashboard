7e61d82549418b70577c8d0fcc3137c82cf5c524
robotscissors <cjbazin@gmail.com>
2018-06-13 23:19:49 -0700

    work in progress - added link and csv - need to focus on data

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -8,7 +8,7 @@ class CampaignsController < ApplicationController
   layout 'admin', only: %i[index create]
   before_action :set_campaign, only: %i[overview programs articles users edit
                                         update destroy add_organizer remove_organizer
-                                        remove_course courses ores_plot articles_csv alerts]
+                                        remove_course courses ores_plot articles_csv all_edits alerts]
   before_action :require_create_permissions, only: [:create]
   before_action :require_write_permissions, only: %i[update destroy add_organizer
                                                      remove_organizer remove_course edit]
@@ -168,6 +168,16 @@ class CampaignsController < ApplicationController
 
   def articles_csv
     filename = "#{@campaign.slug}-articles-#{Time.zone.today}.csv"
+    respond_to do |format|
+      format.csv do
+        send_data CampaignCsvBuilder.new(@campaign).articles_to_csv,
+                  filename: filename
+      end
+    end
+  end
+
+  def all_edits
+    filename = "#{@campaign.slug}-edits-#{Time.zone.today}.csv"
     respond_to do |format|
       format.csv do
         send_data CampaignCsvBuilder.new(@campaign).articles_to_csv,
